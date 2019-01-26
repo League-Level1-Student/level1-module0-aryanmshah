aca04aee39ab27fea228badf9afa8bb9b14c2506
aryanmshah <league@WTS-IM-17.attlocal.net>
2019-01-26 14:27:22 -0800

    urmom

--- src/PhotoQuiz.java
+++ src/PhotoQuiz.java
@@ -22,23 +22,31 @@ public class PhotoQuiz {
                 quizWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   // This will make sure the program exits when you close the window
 
 		// 1. find an image on the internet, and put its URL in a String variable (from your browser, right click on the image, and select “Copy Image Address”)
-
+String a="https://www.crowdrise.com/media/medium/user_photo-584ddb90c07ac.JPG";
 		// 2. create a variable of type "Component" that will hold your image
-
+Component b;
+b=createImage(a);
 		// 3. use the "createImage()" method below to initialize your Component
 
 		// 4. add the image to the quiz window
+quizWindow.add(b);
 
 		// 5. call the pack() method on the quiz window
-
+quizWindow.pack();
 		// 6. ask a question that relates to the image
-
+String d=JOptionPane.showInputDialog("Name the creature.");
 		// 7. print "CORRECT" if the user gave the right answer
-
+if(d.equals("Ponton")) {
+	
+	JOptionPane.showConfirmDialog(null, "Correct");
+}
+else {
+	JOptionPane.showConfirmDialog(null, "Ur wrong idiot");
+}
 		// 8. print "INCORRECT" if the answer is wrong
 
 		// 9. remove the component from the quiz window (you may not see the effect of this until step 12)
-
+quizWindow.remove(b);
 		// 10. find another image and create it (might take more than one line of code)
 
 		// 11. add the second image to the quiz window
